2e3f7ea25d103f6ccd81d343401824090b608514
MohamedCK <abdulhakimm280@gmail.com>
2023-01-03 12:40:11 +0200

    Fix the linter errors

--- student.rb
+++ student.rb
@@ -1,4 +1,4 @@
-require_relative 'person.rb'
+require_relative 'person'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,4 @@
-require_relative 'person.rb'
+require_relative 'person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
