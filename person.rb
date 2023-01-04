cfbbd2cca2a07b236868f313a1cbd330653fef6c
MohamedCK <abdulhakimm280@gmail.com>
2023-01-04 15:22:49 +0200

    - Turn the person class to `nameable`

--- person.rb
+++ person.rb
@@ -1,16 +1,27 @@
-class Person
+# rubocop:disable all
+require 'securerandom'
+
+####  Created a Class Nameable
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+####  Turned my person class to Nameable
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = generate_id
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services?
-    is_of_age? || @parent_permission
+  def correct_name
+    @name
   end
 
   private
@@ -22,4 +33,18 @@ class Person
   def generate_id
     @id = rand(1...1000)
   end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
+
+
+# person = Person.new(18, 'test_subject_1')
+# person.correct_name
+# capitalizedPerson = CapitalizeDecorator.new(person)
+# capitalizedPerson.correct_name
+# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+# capitalizedTrimmedPerson.correct_name
