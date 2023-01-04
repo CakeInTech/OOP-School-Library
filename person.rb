5a51fb523c896cf0aaf3228774483b9c26e18f7e
MohamedCK <abdulhakimm280@gmail.com>
2023-01-04 16:03:40 +0200

    - prepare capitalizeDecorator and TrimmerDecorator

--- person.rb
+++ person.rb
@@ -33,3 +33,16 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
+
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name[0..9]
+  end
+end
