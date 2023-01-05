21d880de76b3227cad96fefc9b49ff9e5ea33e42
MohamedCK <abdulhakimm280@gmail.com>
2023-01-05 22:32:16 +0200

    - Create the app entry point
    - Create the `list_all_books` method

---
+++ app.rb
@@ -0,0 +1,32 @@
+require_relative 'rental'
+require_relative 'person'
+require_relative 'classroom'
+require_relative 'book'
+require_relative 'student'
+require_relative 'teacher'
+
+class App 
+    attr_accessor :books, :people, :rentals
+
+    def initialize 
+        @books = []
+        @people = []
+        @rentals = []
+    end
+
+    def start 
+        puts "Welcome to School Library app!"
+        list_of_options
+    end
+     
+    def list_all_books
+        if @books.length.positive?
+          @books.each do |book|
+            puts "Title: #{book.title}, Author: #{book.author}"
+          end
+        else
+          puts 'No books found'
+        end
+      end
+ 
+end
