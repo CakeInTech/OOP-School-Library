279141faf5b5cb0f35809749472fc00e5af7c3d0
MohamedCK <abdulhakimm280@gmail.com>
2023-01-05 20:27:13 +0200

    - set up many to many relationship for rentals.

--- person.rb
+++ person.rb
@@ -1,19 +1,21 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError
-  end
-end
+require_relative 'nameable.rb'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
+  end
+  
+  def rentals(rental)
+    @rentals << rental
+    rental.person = self
   end
 
   def correct_name
@@ -24,36 +26,22 @@ class Person < Nameable
     is_of_age? || @parent_permission
   end
 
+  def generate_id
+    @id = rand(1...1000)
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 
-  def generate_id
-    @id = rand(1...1000)
-  end
 end
 
-class Decorator < Nameable
-  def initialize(nameable)
-    @nameable = nameable
-    super()
-  end
 
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name[0..9]
-  end
-end
+# person = Person.new(22, 'maximilianus')
+  # person.correct_name
+  # capitalizedPerson = CapitalizeDecorator.new(person)
+  # capitalizedPerson.correct_name
+  # capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  # capitalizedTrimmedPerson.correct_name
