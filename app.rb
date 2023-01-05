f8f4b334c85e5c0501e54454ea35e1dbb11c551b
MohamedCK <abdulhakimm280@gmail.com>
2023-01-05 22:37:15 +0200

    - Make the `create_teacher` and `create-student` method

--- app.rb
+++ app.rb
@@ -49,4 +49,28 @@ class App
         end
     end
 
+    def create_student
+        puts 'Age:'
+        age = gets.chomp
+        puts 'Name:'
+        name = gets.chomp
+        puts 'Has parent permission? [Y/N]'
+        parent_permission = gets.chomp
+        parent_permission = parent_permission.downcase == 'y'
+        student = Student.new(nil, age, name, parent_permission)
+        @people << student
+        puts 'Person(Student) created successfully'
+    end
+
+    def create_teacher
+        puts 'Age:'
+        age = gets.chomp
+        puts 'Name:'
+        name = gets.chomp
+        puts 'Specialization:'
+        specialization = gets.chomp
+        teacher = Teacher.new(age, specialization, name)
+        @people << teacher
+        puts 'Person(Teacher) created successfully'
+    end
 end
