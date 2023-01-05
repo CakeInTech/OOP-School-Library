105cb88095cc77b76853225799cef5fa26b5382d
MohamedCK <abdulhakimm280@gmail.com>
2023-01-05 20:09:30 +0200

    - Create a `classroom` class
    - set up a `setter` and `getter` for @label

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+class Classroom
+    attr_accessor :label
+  
+    def initialize(label)
+      @label = label
+      @students = []
+    end
+  
+    def add_student(student)
+      @students << student
+      student.classroom = self
+    end
+end
+
+  
