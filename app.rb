d8279b4feca7d1276c2054c89e3613bbe563944d
MohamedCK <abdulhakimm280@gmail.com>
2023-01-05 22:33:15 +0200

    - Create the `list_all_people` method

--- app.rb
+++ app.rb
@@ -27,6 +27,12 @@ class App
         else
           puts 'No books found'
         end
+    end
+
+    def list_all_people
+        @people.each do |person|
+          puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
       end
- 
+
 end
