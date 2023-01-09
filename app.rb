67b8e17850f7a269a9a5655838652d96cfc17b2d
MohamedCK <abdulhakimm280@gmail.com>
2023-01-09 21:26:32 +0200

    - format the code

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require_relative 'rental'
-require_relative 'person'
-require_relative 'classroom'
-require_relative 'book'
-require_relative 'student'
-require_relative 'teacher'
+require_relative "rental"
+require_relative "person"
+require_relative "classroom"
+require_relative "book"
+require_relative "student"
+require_relative "teacher"
 
 class App
   attr_accessor :books, :people, :rentals
@@ -15,7 +15,7 @@ class App
   end
 
   def start
-    puts 'Welcome to the Library System!'
+    puts "Welcome to the Library System!"
     list_of_options
   end
 
@@ -25,28 +25,28 @@ class App
         puts "Title: #{book.title}, Author: #{book.author}"
       end
     else
-      puts 'No books found'
+      puts "No books found"
     end
   end
 
   def list_all_people
     if books.empty?
-      puts 'No books found'
+      puts "No books found"
       return
     end
     books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
   end
 
   def create_person
-    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    puts "Do you want to create a student (1) or a teacher (2)? [Input the number]:"
     person_type = gets.chomp
     case person_type
-    when '1'
+    when "1"
       create_student
-    when '2'
+    when "2"
       create_teacher
     else
-      puts 'That is not a valid input'
+      puts "That is not a valid input"
       create_person
     end
   end
@@ -54,58 +54,58 @@ class App
   def create_student
     student_attributes = yoink_student_attributes
     student = Student.new(nil, student_attributes[:age], student_attributes[:name],
-                          student_attributes[:parent_permission])
+      student_attributes[:parent_permission])
     people << student
-    puts 'Person(student) Created successfully'
+    puts "Person(student) Created successfully"
   end
 
   def create_teacher
     teacher_attributes = yoink_teacher_attributes
     teacher = Teacher.new(teacher_attributes[:age], teacher_attributes[:specialization], teacher_attributes[:name])
     people << teacher
-    puts 'Person(Teacher) created successfully'
+    puts "Person(Teacher) created successfully"
   end
 
   def create_book
     book_attributes = yoink_book_attributes
     book = Book.new(book_attributes[:title], book_attributes[:author])
     books << book
-    puts 'Book created!'
+    puts "Book created!"
   end
 
   ##### Book renting
   def create_rental
     book = select_book
     person = select_person
-    puts 'Date:'
+    puts "Date:"
     date = gets.chomp
     @rentals << Rental.new(date, person, book)
-    puts 'Rental created successfully!'
+    puts "Rental created successfully!"
   end
 
   def select_book
-    puts 'Select a book from the following list by number'
+    puts "Select a book from the following list by number"
     @books.each_with_index do |book, index|
       puts "#{index} Title: #{book.title}, Author: #{book.author}"
     end
-    puts 'Book number:'
+    puts "Book number:"
     book_index = gets.chomp.to_i
     @books[book_index]
   end
 
   def select_person
-    puts 'Select a person from the following list by number (not id)'
+    puts "Select a person from the following list by number (not id)"
     @people.each_with_index do |person, index|
       puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
-    puts 'Person number:'
+    puts "Person number:"
     person_index = gets.chomp.to_i
     @people[person_index]
   end
 
   def list_rental_for_person
     id = yoink_id
-    puts 'Rentals:'
+    puts "Rentals:"
     rentals.select { |rental| rental.person.id == id }.each do |rental|
       puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}"
     end
@@ -116,35 +116,35 @@ class App
   private
 
   def yoink_student_attributes
-    puts 'Age'
+    puts "Age"
     age = gets.chomp
-    puts 'Name:'
+    puts "Name:"
     name = gets.chomp
-    puts 'Has parent permission? [Y/N]'
+    puts "Has parent permission? [Y/N]"
     parent_permission = gets.chomp
-    { age: age, name: name, parent_permission: parent_permission.downcase == 'y' }
+    {age: age, name: name, parent_permission: parent_permission.downcase == "y"}
   end
 
   def yoink_teacher_attributes
-    puts 'Age:'
+    puts "Age:"
     age = gets.chomp
-    puts 'Specialization:'
+    puts "Specialization:"
     specialization = gets.chomp
-    puts 'Name:'
+    puts "Name:"
     name = gets.chomp
-    { age: age, specialization: specialization, name: name }
+    {age: age, specialization: specialization, name: name}
   end
 
   def yoink_book_attributes
-    puts 'Title:'
+    puts "Title:"
     title = gets.chomp
-    puts 'Author:'
+    puts "Author:"
     author = gets.chomp
-    { title: title, author: author }
+    {title: title, author: author}
   end
 end
 
 def yoink_id
-  puts 'Enter the ID:'
+  puts "Enter the ID:"
   gets.chomp
 end
