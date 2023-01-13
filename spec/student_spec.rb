e1ec27b2a084e72c022fceb5bef06a06fd200bac
MohamedCK <abdulhakimm280@gmail.com>
2023-01-13 17:14:28 +0200

    - Create a unit test for the student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative '../student'
+
+
+describe Student do
+    let(:classroom) { double('classroom') }
+    let(:student) { Student.new(18, classroom, 'John Doe', true) }
+  
+    describe '#initialize' do
+      it 'sets the classroom' do
+        expect(student.classroom).to eq(classroom)
+      end
+    end
+  
+    describe '#play_hooky' do
+      it 'returns the string "¯\(ツ)/¯"' do
+        expect(student.play_hooky).to eq('¯\(ツ)/¯')
+      end
+    end
+end
